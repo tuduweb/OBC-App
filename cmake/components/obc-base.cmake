3148ad16eb5bf7d2c2c02dd01de0362bd40b820c
bin <tuduweb@qq.com>
2021-10-27 20:37:04 +0800

    构建基础工程。

---
+++ cmake/components/obc-base.cmake
@@ -0,0 +1,9 @@
+set(OBC_BASEDIR_COMPONENTS ${CMAKE_SOURCE_DIR}/src/components)
+set(OBC_BASEDIR_CORE ${CMAKE_SOURCE_DIR}/src/core)
+set(OBC_BASEDIR_BASE ${CMAKE_SOURCE_DIR}/src/base)
+
+set(OBC_BASE_SOURCES
+    #
+    ${CMAKE_SOURCE_DIR}/src/utils/OBCHelpers.cpp
+    ${CMAKE_SOURCE_DIR}/src/utils/OBCHelpers.hpp
+)
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <QDebug>
+
+//#include "utils/OBCHelpers.hpp"
+
+int main(int argc, char *argv[])
+{
+    std::cout << "hello world!" << std::endl;
+    qDebug() << "hello QT5!";
+    return 0;
+}
