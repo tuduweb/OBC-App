961f5b6ab8af57fac9fad9bf57fb2b30a01ceea5
bin <tuduweb@qq.com>
2021-10-30 17:49:00 +0800

    尝试将foundation部分以submodule形式引入工程。

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <QDebug>
 
-//#include "utils/OBCHelpers.hpp"
+#include "utils/OBCHelpers.hpp"
 
 int main(int argc, char *argv[])
 {
     std::cout << "hello world!" << std::endl;
     qDebug() << "hello QT5!";
+
+    qDebug() << Base64Encode("hello world");
+
     return 0;
 }
