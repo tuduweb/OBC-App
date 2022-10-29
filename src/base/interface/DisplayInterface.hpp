c4972890e4f61469b3a537be41b928510dcc8c37
bin <tuduweb@qq.com>
2022-10-29 23:17:16 +0800

    加入帧率统计类

--- src/base/interface/DisplayInterface.hpp
+++ src/base/interface/DisplayInterface.hpp
@@ -2,6 +2,32 @@
 
 #include <QWidget>
 
+#include <QDateTime>
+#include <QVector>
+class FPSCounter
+{
+public:
+	void refreshFPS() {
+		qint64 currentTime = QDateTime::currentDateTime().toMSecsSinceEpoch();
+		m_frames.push_back(currentTime);
+
+		while (m_frames[0] < (currentTime - 1000)) {
+			m_frames.pop_front();
+		}
+
+		int currentCount = m_frames.length();
+		m_value = (currentCount + m_cacheCount) / 2;
+
+		if (currentCount != m_cacheCount) {
+			//emit valueChanged(m_value);
+		}
+
+		m_cacheCount = currentCount;
+	};
+	int m_value = -1;
+	int m_cacheCount = 0;
+	QVector<qint64> m_frames;
+};
 
 
 class DisplayInterface : public QWidget
